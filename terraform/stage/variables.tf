85fcb374e8491f30cfc946400a8f9702577ebe29
oashnic <pavel@sirot.ru>
2019-09-04 13:19:29 +0300

    Add old files

---
+++ terraform/stage/variables.tf
@@ -0,0 +1,50 @@
+variable "project" {
+  description = "ID project"
+}
+
+variable "region" {
+  description = "Region"
+
+  # Значение по умолчанию
+  default = "europe-west1"
+}
+
+variable "zone" {
+  description = "Zone"
+
+  # Значение по умолчанию
+  default = "europe-west1-b"
+}
+
+variable "public_key_path" {
+  # Описание переменной
+  description = "public key appuser"
+}
+
+variable "public_key_path_2" {
+  # Описание переменной
+  description = "public key appuser1"
+}
+
+variable "disk_image" {
+  description = "Disk image"
+}
+
+variable "private_key_path" {
+  description = "~/.ssh/appuser"
+}
+
+variable "count_node" {
+  description = "Count node reddit"
+  default = "1"
+}
+
+variable "app_disk_image" {
+  description = "App disk image"
+  default = "reddit-app-base-1567515065"
+}
+
+variable "db_disk_image" {
+  description = "Disk image for reddit db"
+  default = "reddit-db-base"
+}
