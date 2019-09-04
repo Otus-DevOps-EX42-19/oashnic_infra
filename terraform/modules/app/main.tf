3a0a278637ab7ab5a1894313d390ca9e99710f23
oashnic <pavel@sirot.ru>
2019-09-04 11:52:16 +0300

    Add nginx

--- terraform/modules/app/main.tf
+++ terraform/modules/app/main.tf
@@ -2,16 +2,19 @@ resource "google_compute_instance" "app" {
   name         = "reddit-app"
   machine_type = "g1-small"
   zone         = var.zone
-  tags         = ["reddit-app"]
+  tags         = ["reddit-app", "http-server"]
+
   boot_disk {
     initialize_params { image = var.app_disk_image }
   }
+
   network_interface {
     network = "default"
     access_config {
       nat_ip = google_compute_address.app_ip.address
     }
   }
+
   metadata = {
     ssh-keys = "appuser:${file(var.public_key_path)}"
   }
@@ -26,24 +29,6 @@ resource "google_compute_instance" "app" {
    private_key = file(var.private_key_path)
   }
 
-   # Деплой приложения
-
-   # systemd
-   provisioner "file" {
-    source      = "${path.module}/files/puma.service"
-    destination = "/tmp/puma.service"
-   }
-
-   provisioner "file" {
-     source = "${path.module}/files/deploy.sh"
-     destination = "/tmp/deploy.sh"
-   }
-
-   # скрипт деплоя
-   provisioner "remote-exec" {
-     inline = [ "chmod +x /tmp/deploy.sh",
-    "/tmp/deploy.sh"]
-   }
  }
 
 resource "google_compute_address" "app_ip" {
